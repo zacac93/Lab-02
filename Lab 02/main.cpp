1ca9d84f56a0a1df6b06df43f481e5b41699deaa
Zac Clark <zacclark@zacs-macbook-pro-2.local>
2016-03-06 16:52:24 -0700

    Initial Commit

---
+++ Lab 02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 02
+//
+//  Created by Zac Clark on 3/6/16.
+//  Copyright © 2016 Zac Clark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
